606f070063ade305457eca2b5a652d4909fd00fe
Kevin S <Kevin.Sommer91@freenet.de>
2022-01-19 16:16:19 +0100

    Klasse 'Datenbank' erweitert

--- src/MainPackage/Datenbank.java
+++ src/MainPackage/Datenbank.java
@@ -4,18 +4,40 @@ import java.sql.*;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 
-public class DB_Verbindung {
+public class Datenbank {
 	
-		// JDBC driver Name und Datenbank URL
-		//static final String JDBC_Driver = "org.postgresql.Driver";
-		static final String DB_URL = "jdbc:postgresql://localhost:5432/java_connection_DB";
-
+		// Datenbank URL
+		private String DB_URL;
 		// Datenbank Zugangsdaten
-		static final String DB_USER = "postgres";
-		static final String DB_PASS = "passwort";
+		private String DB_USER;
+		private String DB_PASS;
 		
-				
-		public static void fuehre1StatementAus(String sqlBefehl) {
+		private Connection conn = null;
+		private Statement stmt = null;
+		
+		public Datenbank() {
+			this.DB_URL = "jdbc:postgresql://localhost:5432/java_connection_DB";
+			this.DB_USER = "postgres";
+			this.DB_PASS = "passwort";
+		}
+		
+		public Datenbank(String DB_URL, String DB_USER, String DB_PASS) {
+			this.DB_URL = DB_URL;
+			this.DB_USER = DB_USER;
+			this.DB_PASS = DB_PASS;
+		}
+		
+//		public void verbindung_herrstellen() {
+//			try {
+//				this.conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+//			
+//			} catch (SQLException e) {				
+//				e.printStackTrace();
+//			}
+//		}
+		
+			
+		public void fuehre1StatementAus(String sqlBefehl) {
 			Connection conn = null;
 			Statement stmt = null;
 			
@@ -35,7 +57,7 @@ public class DB_Verbindung {
 				stmt.close();
 				conn.close();
 				System.out.println("\nVerbindung mit Datenbank wurde getrennt!\n");
-							
+				
 				
 			} catch(Exception e) {
 				System.out.println("Es ist ein Fehler aufgetreten");
@@ -43,7 +65,7 @@ public class DB_Verbindung {
 			
 		}// ENDE Methode fuehre1StatementAus()
 		
-		public static void fuehreXStatementsAus(ArrayList<String> befehle) {
+		public void fuehreXStatementsAus(ArrayList<String> befehle) {
 			Connection conn = null;
 			Statement stmt = null;
 			
@@ -73,7 +95,7 @@ public class DB_Verbindung {
 				
 		}// ENDE Methode fuehreXStatementsAus()
 		
-		public static void ausgabeTabelle(String tabelle) {
+		public void ausgabeTabelle(String tabelle) {
 			
 			final String pattern = "000";
 			final DecimalFormat zahlenFormat = new DecimalFormat(pattern);	
--- src/MainPackage/MAIN.java
+++ src/MainPackage/MAIN.java
@@ -15,19 +15,22 @@ public class MAIN {
 		
 		System.out.println("\nDas Programm startet...\n");
 		
-		// Lösche alle Daten aus 'personen'
-		DB_Verbindung.fuehre1StatementAus("DELETE FROM personen");
+		// Erstelle ein Objekt für eine Verbindung mit einer Datenbank
+		Datenbank Postgre_java_conn = new Datenbank(); // Leerer Konstruktor -> default Datenbank (Siehe Klasse 'DB_Verbindung')
+		
+		// Lösche alle Daten aus der Tabelle 'personen'
+		Postgre_java_conn.fuehre1StatementAus("DELETE FROM personen");
 		
 		// Erzeuge Testdaten
-		File testdaten = new File("C:\\Users\\user1\\eclipse-workspace\\Task01_JDBC\\src\\textdateien", "testdaten.txt");				
-		Input_Output_txt.erzeugeTestdaten_txtDatei(testdaten, 100);		
+		File testInputDaten = new File("C:\\Users\\user1\\eclipse-workspace\\Task01_JDBC\\src\\textdateien", "testdaten.txt");				
+		Input_Output_txt.erzeugeTestdaten_txtDatei(testInputDaten, 200);		
 		
 		// erstelle Scanner Objekt
 		Scanner scan = null;
 		
 		// lese Datei 'testdaten'(Objekt 'testdaten') ein
 		try {
-			scan = new Scanner(testdaten);
+			scan = new Scanner(testInputDaten);
 			//scan.close();
 		} catch (FileNotFoundException e) {			
 			System.out.println("Datei nicht vorhanden");
@@ -57,18 +60,20 @@ public class MAIN {
 		ArrayList<String> statements = new ArrayList<String>();
 		for (Person person : listePersonen) {
 			StringBuilder sb = new StringBuilder("INSERT INTO personen(p_id, p_vorname, p_nachname, p_alter) VALUES");
-			sb.append(MessageFormat.format("({0}, {1}, {2}, {3})", i_id, DB_Verbindung.sqlString(person.getVorname()), DB_Verbindung.sqlString(person.getNachname()), person.getAlter()));			
+			sb.append(MessageFormat.format("({0}, {1}, {2}, {3})", 
+					i_id, Datenbank.sqlString(person.getVorname()), Datenbank.sqlString(person.getNachname()), person.getAlter()));			
 			String statement = sb.toString();
 			//String statement = MessageFormat.format("INSERT INTO personen(p_id, p_vorname, p_nachname, p_alter) VALUES({0},\'{1}\',\'{2}\',{3})",i_id, person.getVorname(), person.getNachname(), person.getAlter());
 			statements.add(statement);
 			i_id++;
 		}
+		
 		// Führe die Statements aus
-		DB_Verbindung.fuehreXStatementsAus(statements);
+		Postgre_java_conn.fuehreXStatementsAus(statements);
 		
 		// Konsolenausgabe aller Daten in der Tabelle 'personen'
 		System.out.println("Ausgabe aller Daten in der Tabelle 'personen':\n");
-		DB_Verbindung.ausgabeTabelle("personen");
+		Postgre_java_conn.ausgabeTabelle("personen");
 		
 		
 		
