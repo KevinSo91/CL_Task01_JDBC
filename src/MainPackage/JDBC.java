a9c6151dd75b7f99ba4177f002aa7c93a0ee98f7
Kevin S <Kevin.Sommer91@freenet.de>
2022-01-19 14:05:26 +0100

    Test -> JDBC

--- src/MainPackage/JDBC.java
+++ src/MainPackage/JDBC.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 import java.sql.*;
 //import java.util.concurrent.ThreadLocalRandom;
 import java.util.Random;
+import java.text.*;
 
 
 
@@ -22,45 +23,25 @@ public class JDBC {
 	static final String DB_USER = "postgres";
 	static final String DB_PASS = "passwort";
 	
+	static final String pattern = "000";
+	static final DecimalFormat zahlenFormat = new DecimalFormat(pattern);	
 	
 	// main Methode
 	public static void main(String[] args) {
 		
 		System.out.println("\nDas Programm startet...\n");
 		
+		// Lösche alle Daten aus 'personen'
 		fuehre1StatementAus("DELETE FROM personen");
-				
-		//erzeugeTestdaten_txtDatei(500);												
-		// ***************************************************************************************************// Testdaten erzeugen
-		final String pathname = "C:\\Users\\user1\\eclipse-workspace\\Task01_JDBC\\src\\textdateien";
-		final String dateiname = "Testdaten";
 		
-		// Erstelle eine TXT-Datei
-		File testdaten = new File(pathname, dateiname + ".txt");
-		try {
-			//FileWriter writer = new FileWriter(testdaten, /*append:*/true);
-			FileWriter writer = new FileWriter(testdaten);    // -> Datei überschreiben
-			for(int i = 0; i < 200; i++) {
-				// Zufälliges Alter erzeugen
-				//int zufallAlter = ThreadLocalRandom.current().nextInt(18,68);
-				Random random = new Random();
-				int zufallAlter = random.nextInt(67-18)+18;  //(max-min)+min
-				
-				writer.write("Vorname" + i  + " Nachname" + i + " "+ zufallAlter + "\n");
-			}
-			writer.flush();
-			writer.close();
-		} catch (IOException e) {
-			System.out.println("Fehlerhafte Eingabe");
-			e.printStackTrace();
-		}
-		// ***************************************************************************************************// Testdaten erzeugen
-		
+		// Erzeuge Testdaten
+		File testdaten = new File("C:\\Users\\user1\\eclipse-workspace\\Task01_JDBC\\src\\textdateien", "testdaten.txt");				
+		erzeugeTestdaten_txtDatei(testdaten, 500);		
 		
 		// erstelle Scanner Objekt
 		Scanner scan = null;
 		
-		// lese Datei 'Testdaten'(Objekt 'testdaten') ein
+		// lese Datei 'testdaten'(Objekt 'testdaten') ein
 		try {
 			scan = new Scanner(testdaten);
 			//scan.close();
@@ -84,42 +65,47 @@ public class JDBC {
 		}
 		
 		// Gib die Attribute der einzelnen Objekte in 'listePersonen' in der Konsole aus
+		System.out.println("Ausgabe der Objekte in 'listePersonen':\n");
 		listePersonen.forEach((person) -> person.infoAusgeben());
 		
-		// Schreibe die Attribute der Objekte in die DB
+		// Erstelle eine ArrayList mit den Statements zum einfügen der Objekte in die DB
 		int i_id = 1;
 		ArrayList<String> statements = new ArrayList<String>();
 		for (Person person : listePersonen) {
-			statements.add("INSERT INTO personen(p_id, p_vorname, p_nachname, p_alter) VALUES(" + i_id + ", '" + person.getVorname() + "', '" + person.getNachname() + "', " + person.getAlter() + ")");
+			StringBuilder sb = new StringBuilder("INSERT INTO personen(p_id, p_vorname, p_nachname, p_alter) VALUES");
+			sb.append(MessageFormat.format("({0}, {1}, {2}, {3})", i_id, sqlString(person.getVorname()), sqlString(person.getNachname()), person.getAlter()));			
+			String statement = sb.toString();
+			//String statement = MessageFormat.format("INSERT INTO personen(p_id, p_vorname, p_nachname, p_alter) VALUES({0},\'{1}\',\'{2}\',{3})",i_id, person.getVorname(), person.getNachname(), person.getAlter());
+			statements.add(statement);
 			i_id++;
 		}
+		// Führe die Statements aus
 		fuehreXStatementsAus(statements);
+		
+		// Konsolenausgabe aller Daten in der Tabelle 'personen'
+		System.out.println("Ausgabe aller Daten in der Tabelle 'personen':\n");
+		ausgabeTabelle("personen");
 		
 		
 		
 		System.out.println("\nDas Programm wird beendet...");
 		
-	}// Ende 'main'
+	}// ENDE Methode main()
 					
 	
 		
-	public static void erzeugeTestdaten_txtDatei(int anzahlZeilen) {
+	public static void erzeugeTestdaten_txtDatei(File datei, int anzahlZeilen) {
 		
-		// Gebe das Verzeichnis und den Namen der Datei an, die erstellt werden soll
-		final String pathname = "C:\\Users\\user1\\eclipse-workspace\\Task01_JDBC\\src\\textdateien";
-		final String dateiname = "Testdaten";
-				
-		// Erstelle eine TXT-Datei
-		final File testdaten = new File(pathname, dateiname + ".txt");
+		
 		try {
-			//FileWriter writer = new FileWriter(testdaten, /*append:*/true);
-			FileWriter writer = new FileWriter(testdaten);    // -> Datei überschreiben
+			//FileWriter writer = new FileWriter(datei, /*append:*/true); // -> Datei erweitern
+			FileWriter writer = new FileWriter(datei);    // -> Datei überschreiben
 			// Schreibe in jede Zeile eine Person mit zufälligem Alter zwischen 18(inklusiv) und 68(exklusiv)
 			for(int i = 0; i < anzahlZeilen; i++) {
 				// Zufälliges Alter erzeugen
 				Random random = new Random();
-				int zufallAlter = random.nextInt(67-18);
-				writer.write("Vorname" + i  + " Nachname" + zufallAlter + "\n");
+				int zufallAlter = random.nextInt(68-18)+18; // (max(exklusiv)-min(inklusiv))-min
+				writer.write("Vorname" + i  + " Nachname" + i + " " + zufallAlter + "\n");
 			}
 			writer.flush();
 			writer.close();
@@ -127,8 +113,8 @@ public class JDBC {
 			System.out.println("Fehlerhafte Eingabe");
 			e.printStackTrace();
 		}
-				
-	}// Ende 'erzeugeTestdaten_txtDatei' Methode
+		
+	}// ENDE Methode erzeugeTestdaten_txtDatei()
 
 	public static void fuehre1StatementAus(String sqlBefehl) {
 		Connection conn = null;
@@ -136,7 +122,7 @@ public class JDBC {
 		
 		try {
 			// 1. Verbindung herstellen
-			System.out.println("Verbinde mit Datenbank...");
+			System.out.println("\nVerbinde mit Datenbank...");
 			conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
 			System.out.println("Erfolgreich mit Datenbank verbunden!");
 			
@@ -149,13 +135,14 @@ public class JDBC {
 			// 3. Verbindung trennen
 			stmt.close();
 			conn.close();
-			System.out.println("Verbindung mit Datenbank wurde getrennt!");
+			System.out.println("\nVerbindung mit Datenbank wurde getrennt!\n");
 						
 			
 		} catch(Exception e) {
 			System.out.println("Es ist ein Fehler aufgetreten");
 		}
-	}// Ende 'fuehre1StatementAus'
+		
+	}// ENDE Methode fuehre1StatementAus()
 	
 	public static void fuehreXStatementsAus(ArrayList<String> befehle) {
 		Connection conn = null;
@@ -163,12 +150,12 @@ public class JDBC {
 		
 		try{
 			// 1. Verbindung herstellen
-			System.out.println("Verbinde mit Datenbank...");
+			System.out.println("\nVerbinde mit Datenbank...");
 			conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
 			System.out.println("Erfolgreich mit Datenbank verbunden!\n");
 			
 			// 2. Führe Statement aus
-			System.out.println("Fuehre SQL Statement aus...");
+			System.out.println("Fuehre SQL Statements aus...");
 			for (String befehl : befehle) {
 				System.out.println(befehl);
 				stmt = conn.createStatement();				
@@ -179,13 +166,48 @@ public class JDBC {
 			// 3. Verbindung trennen
 			stmt.close();
 			conn.close();
-			System.out.println("\nVerbindung mit Datenbank getrennt!");
+			System.out.println("\nVerbindung mit Datenbank getrennt!\n");
 			
 		} catch(Exception e) {
 			System.out.println("Es ist ein Fehler aufgetreten");
 		}
 			
-	}// Ende 'fuehreXStatementsAus' 
+	}// ENDE Methode fuehreXStatementsAus() 
 	
+	public static void ausgabeTabelle(String tabelle) {
+		Connection conn = null;
+		Statement stmt = null;
+		
+		try{
+			conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+			stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM " + tabelle + " ORDER BY p_alter"); // Sortierung nach Alter
+			while (rs.next()) {
+				int id = rs.getInt("p_id");
+				String vorname = rs.getString("p_vorname");
+				String nachname = rs.getString("p_nachname");
+				int alter = rs.getInt("p_alter");
+				System.out.println("ID: "+ zahlenFormat.format(id) + " | Vorname: " + vorname + " | Nachname: " + nachname + " | Alter: " + alter);
+				System.out.println();
+			}
+			rs.close();
+			stmt.close();
+			conn.close();
+			
+		} catch (Exception e) {
+			System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+	         System.exit(0);
+		}
+		
+	}// ENDE Methode ausgabeTabelle()
 	
-}// Ende 'main' class
+	public static String sqlString(String eingabe)
+	{
+		StringBuilder sb = new StringBuilder(eingabe);
+		sb.insert(0, "'");
+		sb.insert(eingabe.length()+1, "'");		
+		
+		return sb.toString();
+	}
+	
+}// ENDE Klasse 'main'
